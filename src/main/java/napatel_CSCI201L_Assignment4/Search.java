1c53cbfc4909b7aa92c2f9c75ca702650d7e63da
Nikhil Patel <napatel@usc.edu>
2021-04-14 00:17:29 -0700

    if you don't type in a real stock, it will yell at you. If you do, it is ready to move on
    ready to do formatting of recieved stock data

--- src/main/java/napatel_CSCI201L_Assignment4/Search.java
+++ src/main/java/napatel_CSCI201L_Assignment4/Search.java
@@ -2,11 +2,19 @@ package napatel_CSCI201L_Assignment4;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
 
 @WebServlet("/Search")
 public class Search extends HttpServlet {
@@ -27,11 +35,40 @@ public class Search extends HttpServlet {
 		String query = request.getParameter("query");
 		PrintWriter out = response.getWriter();
 		response.setContentType("application/json");
-		out.println("{\"resText\": \"hello morl\"}");
+		out.println(getStockDetails(query));
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		doGet(request, response);
 	}
+	
+	private static String getStockDetails(String ticker) throws IOException{
+        URL url = new URL("https://api.tiingo.com/tiingo/daily/" + ticker + "/prices?token=55db81be5a6b3afad4ecb01acfc13c73204c0ad0");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Accept", "application/json");
+
+        //Case when ticker does not exist
+        if (con.getResponseCode() == 404) {
+            System.out.println(ticker + "72163792167936912 does not exist. All involved trades will be purged.");
+            return "{\"error\": \"true\", \"errorMessage\":\"Ticker "+ticker.toUpperCase()+" does not exist\"}";
+        } else {
+            try {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                return response.toString();
+            } catch (Exception e) {
+            	e.printStackTrace();
+                System.out.println(ticker + " does not exist. All involved trades will be purged.");
+                return "{\"error\": \"true\", \"errorMessage\":\"Ticker "+ticker.toUpperCase()+" does not exist\"}";
+            }
+        }
+    }
 }
