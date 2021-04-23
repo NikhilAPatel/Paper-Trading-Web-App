eac8ee99e50b67744f21b96a90c4a04ec8c5a6b2
Nikhil Patel <napatel@usc.edu>
2021-04-23 14:15:09 -0700

    Selling works but needs to be tested

--- src/main/java/napatel_CSCI201L_Assignment4/Sell.java
+++ src/main/java/napatel_CSCI201L_Assignment4/Sell.java
@@ -70,19 +70,20 @@ public class Sell extends HttpServlet {
 		}
 		
 		//Return error if the market is closed
-		if(marketClosed()) {
-			out.println("{\"success\": false, \"message\": \"FAILED: Market is closed\"}"); //TODO lint
-			return;//TODO can you do this in Java
-		}
-		
-			
-		//Do sell
-		float bid = (float) getStockAttribute(ticker, "bid");
-		if(bid==-1) {
-			out.println("{\"success\": false, \"message\": \"FAILED\"}");
-		}else if(bid==0) {
-			bid = (float) getStockAttribute(ticker, "last");
-		}
+//		if(marketClosed()) {
+//			out.println("{\"success\": false, \"message\": \"FAILED: Market is closed\"}"); //TODO lint
+//			return;//TODO can you do this in Java
+//		}
+//		
+//			
+//		//Do sell
+//		float bid = (float) getStockAttribute(ticker, "bid");
+//		if(bid==-1) {
+//			out.println("{\"success\": false, \"message\": \"FAILED\"}");
+//		}else if(bid==0) {
+//			bid = (float) getStockAttribute(ticker, "last");
+//		}
+		float bid =100;
 		out.println(sell(user_id, ticker, bid, quantity));
 				
 	}
@@ -92,9 +93,6 @@ public class Sell extends HttpServlet {
 	public String sell(int user_id, String ticker, float bid, int quantity) {
 		//Initialization
 		float balance = getUserBalance(user_id);
-		Connection conn = null;
-		PreparedStatement ps = null;
-		int rs;
 		
 		//get all of this stock that this user owns
 		ArrayList<OwnedStock> ownedStock = getOwnedStock(user_id, ticker);
@@ -114,23 +112,56 @@ public class Sell extends HttpServlet {
 		//Calculate the user's new balance
 		float newBalance = balance+(bid*quantity);
 		
+		int entries_to_sell=0;
+		int overflow=0;
+		
+		for(OwnedStock s: ownedStock) {
+			if(quantity==0) {
+				break;
+			}
+			if(quantity>=s.getQuantity()) {
+				//Sell all of it and decrease quantity
+				quantity-=s.getQuantity();
+				boolean success  = sellStock(s.getOs_id());
+				if(!success) {
+					return "{\"success\": false, \"message\": \"FAILED\"}";
+				}
+			}else {
+				//Sell part of it and end
+				boolean success = sellPartialStock(s.getOs_id(), s.getQuantity()-quantity);
+				if(!success) {
+					return "{\"success\": false, \"message\": \"FAILED\"}";
+				}
+			}
+		}
+		
+		//If the code is here, we probably succeeded
+		return "{\"success\": true, \"message\": \"SUCCESS: Executed purchase of "+quantity+" shares of "+ticker+" for $"+round(bid)+"\"}";
+	}
+	
+	public static boolean sellStock(int os_id) {
+		//Initialization
+		Connection conn = null;
+		PreparedStatement ps = null;
+		int rs;
+		
 		//Do the actual sale
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(dbAddress);
-
+	
 			
-			ps = conn.prepareStatement("");
+			ps = conn.prepareStatement("delete from Owned_Stock where os_id=?");
+			ps.setInt(1, os_id);
 			
 			rs = ps.executeUpdate();
 			
 			if(rs==0) {//It probably failed
-				return "{\"success\": false, \"message\": \"FAILED\"}";//TODO lint and check if this actually happens when it fails
+				return false;
 			}
 			
 			//Return success message
-			return "{\"success\": true, \"message\": \"SUCCESS: Executed sale of 2 shares of "+ticker+" for $"+round(bid)+"\"}";//TODO lint
-			
+			return true;
 			
 		} catch (SQLException | ClassNotFoundException sqle) {
 			sqle.printStackTrace();
@@ -147,10 +178,52 @@ public class Sell extends HttpServlet {
 				System.out.println(sqle.getMessage());
 			}
 		}
+		return false;
+	}
+	
+	public static boolean sellPartialStock(int os_id, int quantity) {
+		//Initialization
+		Connection conn = null;
+		PreparedStatement ps = null;
+		int rs;
 		
-		
-		//If the code is here, we probably failed
-		return "{\"success\": false, \"message\": \"FAILED\"}";//TODO lint
+		//Do the actual sale
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbAddress);
+	
+			
+			ps = conn.prepareStatement("update Owned_Stock set quantity=? where os_id=?");
+			ps.setInt(1, quantity);
+			ps.setInt(2,  os_id);
+			
+			rs = ps.executeUpdate();
+			
+			System.out.println(rs);
+			
+			if(rs==0) {//It probably failed
+				return false;
+			}
+			
+			//Return success message
+			return true;
+			
+		} catch (SQLException | ClassNotFoundException sqle) {
+			sqle.printStackTrace();
+			System.out.println(sqle.getMessage());
+		} finally {
+			try {
+				if (ps != null) {
+					ps.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException sqle) {
+				System.out.println(sqle.getMessage());
+			}
+		}
+		return false;
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
