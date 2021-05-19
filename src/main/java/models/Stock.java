2a76e66359c6e9e485f18b5d6cbff2c2225b3cc0
Nikhil Patel <napatel@usc.edu>
2021-05-18 19:46:32 -0700

    Updated pom.xml and Procfile, deleted unused imports

--- src/main/java/models/Stock.java
+++ src/main/java/models/Stock.java
@@ -11,9 +11,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalTime;
 import java.util.Date;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormatter;
-import org.joda.time.format.ISODateTimeFormat;
+
 
 
 public class Stock {
